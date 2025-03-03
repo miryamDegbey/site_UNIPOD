cdb9b0c99f99337230fc65a61b8ead4478f81bba
BorisADN <edemwomagno@gmail.com>
2025-03-03 17:28:34 +0000

    03/03/2025 17:30

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,30 +9,48 @@
         <!-- Swiper -->
         <div class="swiper-container">
             <div class="swiper-wrapper">
-                {{-- <div class="swiper-slide" style="background-image: url('{{ asset('assets/images/photo_banner6.jpeg') }}');">
-                    <div class="vertical-bar"></div>
-                    <div class="text-box">
-                        <p>Texte pour le premier slide</p>
-                    </div>
-                </div> --}}
-
                 <div class="swiper-slide" style="background-image: url('{{ asset('assets/images/photo_banner6.jpeg') }}');">
                     <div class="content">
                         <div class="vertical-bar"></div>
-                        <p>Texte pour le premier slide</p>
+                        <div>
+                            <p class="text-box">Connecter , creer ,innover l'Unipod , Moteur</p>
+                            <p class="text-box">du changement pour</p>
+                            <p class="text-box">jeunesse togolaise !</p>
+                        </div>
                     </div>
                 </div>
 
+                {{-- <div class="swiper-slide" style="background-image: url('{{ asset('assets/images/photo_banner6.jpeg') }}');">
+                    <div class="content">
+                        <div class="vertical-bar">
+                        </div>
+                      <p class="text-box">snkkkkkkkkkkkkkkk</p>
+                      
+                    </div>
+                </div> --}}
+
                 <!-- Slide 2 -->
                 <div class="swiper-slide" style="background-image: url('{{ asset('assets/images/photo_banner7.jpeg') }}');">
-                    <div class="vertical-bar"></div>
-                    
+                    <div class="content">
+                        <div class="vertical-bar"></div>
+                        <div>
+                            <p class="text-box">Lorem ipsum, dolor sit amet consectetur.</p>
+                            <p class="text-box">Lorem ipsum dolor sit amet .</p>
+                            <p class="text-box">Lorem ipsum dolor sit.</p>
+                        </div>
+                    </div>
                 </div>
 
                 <!-- Slide 3 -->
                 <div class="swiper-slide" style="background-image: url('{{ asset('assets/images/photo_banner8.jpeg') }}');">
-                    <div class="vertical-bar"></div>
-                   
+                    <div class="content">
+                        <div class="vertical-bar"></div>
+                        <div>
+                            <p class="text-box">Lorem ipsum, dolor sit amet consectetur.</p>
+                            <p class="text-box">Lorem ipsum dolor sit amet .</p>
+                            <p class="text-box">Lorem ipsum dolor sit.</p>
+                        </div>
+                    </div>
                 </div>
             </div>
             <!-- Boutons de navigation -->
