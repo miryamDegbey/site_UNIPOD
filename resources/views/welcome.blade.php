383835304a6db884ab152d73d8d7a543b5b4711a
BorisADN <edemwomagno@gmail.com>
2025-03-05 18:26:36 +0000

    18/26

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,21 +13,12 @@
                     <div class="contenu">
                         <div class="vertical-bar"></div>
                         <div>
-                            <p class="text-box">Connecter , creer ,innover l'Unipod , Moteur</p>
+                            <p class="text-box">Connecter , créer ,innover l'Unipod , Moteur</p>
                             <p class="text-box">du changement pour</p>
                             <p class="text-box">jeunesse togolaise !</p>
                         </div>
                     </div>
                 </div>
-
-                {{-- <div class="swiper-slide" style="background-image: url('{{ asset('assets/images/photo_banner6.jpeg') }}');">
-                    <div class="contenu">
-                        <div class="vertical-bar">
-                        </div>
-                      <p class="text-box">snkkkkkkkkkkkkkkk</p>
-                      
-                    </div>
-                </div> --}}
 
                 <!-- Slide 2 -->
                 <div class="swiper-slide" style="background-image: url('{{ asset('assets/images/photo_banner7.jpeg') }}');">
@@ -58,51 +49,123 @@
     </div> 
 </div> 
 
+
 <section id="featured-services" class="featured-services section">
-
     <div class="container">
+      <style>
+        .featured-services .swiper {
+          padding-top: 50px
+        }
+      </style>
 
-      <div class="row gy-4">
+<style>
+  .featured-services .service-item {
+    /* border: 1px solid #c10d0d; */
+    /* height: 500px; */
+  }
+</style>
+      
+        <div class="swiper mySwiper">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <div class="service-item position-relative">
+                        <div class="icon"><img src="{{ asset('assets/images/icon1.png')}}" alt="" width="20%"></div>
+                        <h4><a href="#" class="stretched-link service-toggle" data-target="dropdown1">Formation et Mentorat</a></h4>
+                        <div class="dropdown-content" id="dropdown1">
+                            <p>Nous offrons des formations et du mentorat pour aider à développer vos compétences.</p>
+                        </div>
+                    </div>
+                </div>
 
-        <div class="col-lg-4 d-flex" data-aos="fade-up" data-aos-delay="100">
-          <div class="service-item position-relative">
-            <div class="icon"><img src="{{  asset('assets/images/icon1.png')}}" alt="" width="20%"></div>
-            <h4><a href="" class="stretched-link">Formation et Mentorat</a></h4>
-            
-          </div>
-        </div><!-- End Service Item -->
+                <div class="swiper-slide">
+                    <div class="service-item position-relative">
+                        <div class="icon"><img src="{{ asset('assets/images/icon2.png')}}" alt="" width="20%"></div>
+                        <h4><a href="" class="stretched-link">Recherche et développement</a></h4>
+                    </div>
+                </div>
 
-        <div class="col-lg-4 d-flex" data-aos="fade-up" data-aos-delay="200">
-          <div class="service-item position-relative">
-          
-            <div class="icon"><img src="{{  asset('assets/images/icon2.png')}}" alt="" width="20%"></div>
-            <h4><a href="" class="stretched-link">Recherche et développement</a></h4>
-            
-          </div>
-        </div><!-- End Service Item -->
+                <div class="swiper-slide">
+                    <div class="service-item position-relative">
+                        <div class="icon"><img src="{{ asset('assets/images/icon3.png')}}" alt="" width="20%"></div>
+                        <h4><a href="" class="stretched-link">Développement de produits et propriété intellectuelle</a></h4>
+                    </div>
+                </div>
 
-        <div class="col-lg-4 d-flex" data-aos="fade-up" data-aos-delay="300">
-          <div class="service-item position-relative">
-            
-            <div class="icon"><img src="{{  asset('assets/images/icon3.png')}}" alt="" width="20%"></div>
-            <h4><a href="" class="stretched-link">Développement de produits et proprieté intellectuelle</a></h4>
-            
-          </div>
-        </div><!-- End Service Item -->
+                <div class="swiper-slide">
+                    <div class="service-item position-relative">
+                        <div class="icon"><img src="{{ asset('assets/images/icon3.png')}}" alt="" width="20%"></div>
+                        <h4><a href="" class="stretched-link">Développement de produits et propriété intellectuelle</a></h4>
+                    </div>
+                </div>
+            </div>
 
-        {{-- <div class="col-lg-4 d-flex" data-aos="fade-up" data-aos-delay="300">
-          <div class="service-item position-relative">
-            <div class="icon"><i class="bi bi-calendar4-week icon"></i></div>
-            <h4><a href="" class="stretched-link">Incubation et accélération de startups</a></h4>
-            <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p>
-          </div>
-        </div><!-- End Service Item --> --}}
-
-      </div>
-
+            <!-- Navigation buttons -->
+            <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
+        </div>
     </div>
+</section>
 
-  </section>
+<!-- Swiper JS -->
+<script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
+
+<script>
+    document.querySelectorAll(".service-toggle").forEach(item => {
+        item.addEventListener("click", function(event) {
+            event.preventDefault();
+            let target = this.getAttribute("data-target");
+            let dropdown = document.getElementById(target);
+            
+            // Fermer les autres dropdowns
+            document.querySelectorAll(".dropdown-content").forEach(d => {
+                if (d.id !== target) {
+                    d.classList.remove("active");
+                }
+            });
+
+            // Toggle l'affichage du dropdown sélectionné
+            dropdown.classList.toggle("active");
+        });
+    });
+</script>
+
+<!-- Styles pour le dropdown -->
+<style>
+    .dropdown-content {
+        display: none;
+        background: #f8f9fa;
+        padding: 10px;
+        border-radius: 5px;
+        position: relative;
+        width: 100%;
+        top: 100%;
+        left: 0;
+        box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+        transition: all 0.3s ease-in-out;
+    }
+
+    .dropdown-content.active {
+        display: block;
+    }
+
+    .service-item {
+        position: relative;
+    }
+</style>
+
+<script>
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 3, // Affiche 3 services à la fois
+        spaceBetween: 30, // Espace entre les services
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        loop: true, // Active le défilement infini
+    });
+</script>
+
+
 
 
   <section id="about" class="about section">
